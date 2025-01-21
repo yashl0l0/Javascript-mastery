35ee519d89305b3cdf3e900f2f23fce9fdcb04ea
Yash Abhangrao👽 <132886827+yashl0l0@users.noreply.github.com>
2025-01-21 19:57:11 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //Treats all the JS code as a Newer version
+// Code readability should on priority E.g Spacing, Indentation etc
+
+/*
+     ---Data types in Javascript---
+
+    number = 10
+    string = "yash"
+    bigint // for Big No.
+    boolean = true/false
+    null = standalone value, but it is an object in javascript(exception)
+    undefined = it lies under the undefined state
+    object
+*/
