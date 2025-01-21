bd458c07c17783d15ee1af3c3af00cdd1427705d
Yash Abhangrao👽 <132886827+yashl0l0@users.noreply.github.com>
2025-01-21 19:52:22 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14435
+let accountEmail = 'yash@gmail.com';
+var accountPassword = "12345"
+accountCity = "Nashik" 
+
+//accountId = 24568
+/*Prefer not to use "var"
+Due to issue of functional and block scope
+*/
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword]);
+
